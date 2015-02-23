a674b6282e28c5d807ae7966aee8ad6701ca860c
zjpierson <zachary.pierson@mines.sdsmt.edu>
2015-02-22 17:57:10 -0700

    started on simulated_annealing

--- include/simulated_annealing.h
+++ include/simulated_annealing.h
@@ -1,8 +1,13 @@
 #ifndef _SIMULATED_ANNEALING_H_
 #define _SIMULATED_ANNEALING_H_
 
+#include <boost/random.hpp>
+#include <cstdlib>
+#include <iostream>
 
 
+double pertubation( double individual);
+double random( double min, double max);
 
 
 
--- src/simulated_annealing.cpp
+++ src/simulated_annealing.cpp
@@ -1,1 +1,14 @@
 #include "../include/simulated_annealing.h"
+
+
+double pertubation( double individual)
+{
+
+    return random(0,1);
+
+}
+
+inline double random( double min, double max)
+{
+    return ((double)rand()/(double)RAND_MAX)*(max-min);
+}
