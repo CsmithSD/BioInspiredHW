b9e3949b17e76e306da060d572cddbfd57c62769
Zachary Pierson <zachary.pierson@mines.sdsmt.edu>
2015-02-25 11:39:00 -0700

    fixed name in fit_func.h and finished simulated_annealing.cpp

--- include/fit_func.h
+++ include/fit_func.h
@@ -3,6 +3,6 @@
 #ifndef _FIT_FUNC_H_
 #define _FIT_FUNC_H_
 
-double fit_func(const double &x );
+double fit(const double &x );
 
 #endif
--- src/simulated_annealing.cpp
+++ src/simulated_annealing.cpp
@@ -1,14 +1,66 @@
-#include "../include/simulated_annealing.h"
+//#include <boost/random.hpp>
+#include <cstdlib>
+#include <iostream>
+#include "../include/fit_func.h"
+
+#define GEN 500
+
+//function prototypes 
+double pertubation( double individual);
+double random( double min, double max);
+void initialize_T(double & t);
+void update(double & T);
+
+using namespace std;
+
+int main()
+{
+    //Seed random number generator
+    srand(time(NULL));
+
+    //initialize individual inbetween -1 and 1 
+    double x = random(-1, 1);
+    double xprime;      //next pertubation of individual
+    double T = 1.0;     //initialize T (temperature) at a higher value
+
+    //Loop though the number of generations
+    for(int i = 0; i < GEN; i++)
+    {
+        //pertubation of the individual
+        xprime = pertubation(x);
+
+        //evaluation of the individuals using T
+        if( random(0, 1) < exp( (fit(xprime) - fit(x))/T ) )
+        {
+            cout << i << ": " << fit(xprime) - fit(x) << endl;
+            x = xprime;
+        }
+        
+        update(T);
+    }
+
+    cout << x << " : " << fit(x) << endl;
+    
+    return 0;
+}
 
 
 double pertubation( double individual)
 {
-
-    return random(0,1);
-
+    return individual + random(-.2, .2);
 }
 
 inline double random( double min, double max)
 {
-    return ((double)rand()/(double)RAND_MAX)*(max-min);
+    return ((double)rand()/(double)RAND_MAX)*(max-min) + min;
+}
+
+void initialize_T(double & t)
+{
+    t = 1.00;
+}
+
+void update(double & t)
+{
+    t = ( t* (.95) );
 }
